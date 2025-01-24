74cff1cf3b8c5422022d142ff2419bc3152a5c32
Shehroz Mukhtar <shehroz2260@gmail.com>
2025-01-24 09:49:48 +0500

    handle loading

--- lib/view/main_view/main_view.dart
+++ lib/view/main_view/main_view.dart
@@ -32,6 +32,13 @@ class _MainViewState extends State<MainView> {
     super.initState();
   }
 
+  @override
+  void dispose() {
+    scrollController.removeListener(onScroll);
+    scrollController.dispose();
+    super.dispose();
+  }
+
   loadAPi() async {}
   @override
   Widget build(BuildContext context) {
@@ -72,66 +79,82 @@ class _MainViewState extends State<MainView> {
               const SizedBox(
                 height: 20,
               ),
-              Expanded(child: BlocBuilder<MainBloc,MainState>(
+              Expanded(child: BlocBuilder<MainBloc, MainState>(
                 builder: (context, state) {
-                  return state.isLoading? const Center(child: CircularProgressIndicator()):  ListView.builder(
-                    itemCount: state.userModel.length,
-                      controller: scrollController,
-                      itemBuilder: (context, index) {
-                        final user = state.userModel[index];
-                        return Container(
-                          margin: const EdgeInsets.only(bottom: 20),
-                          decoration: BoxDecoration(
-                            color: AppColors.lightGrey,
-                            borderRadius: BorderRadius.circular(20),
-                          ),
-                          padding: const EdgeInsets.all(15),
-                          child:  Column(
-                            children: [
-                              Row(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceBetween,
-                                children: [
-                                  Text(user.nat),
-                                  const Icon(Icons.more_vert)
-                                ],
+                  return state.isLoading && state.page == 1
+                      ? const Center(child: CircularProgressIndicator())
+                      : ListView.builder(
+                          itemCount: state.userModel.length,
+                          controller: scrollController,
+                          itemBuilder: (context, index) {
+                            final user = state.userModel[index];
+
+                            return Container(
+                              margin: const EdgeInsets.only(bottom: 20),
+                              decoration: BoxDecoration(
+                                color: AppColors.lightGrey,
+                                borderRadius: BorderRadius.circular(20),
                               ),
-                              const SizedBox(height: 10,),
-                              const Divider(
-                                height: 0,
-                                thickness: 1,
-                                color: AppColors.white,
-                              ),
-                              Row(
+                              padding: const EdgeInsets.all(15),
+                              child: Column(
                                 children: [
-                                  AppCacheImage(imageUrl: user.picture.medium,height: 60,round: 60,),
-                                  const SizedBox(width: 10),
-                                  Text("${user.name.title}, ${user.name.first} ${user.name.last} - ${user.dob.age}"),
-                                ],
-                              ),
-                              const SizedBox(height: 10,),
-                              Row(
-                                children: [
-                                  Expanded(child: Column(
+                                  Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
                                     children: [
-                                      Text(user.dob.date),
+                                      Text(user.nat),
+                                      const Icon(Icons.more_vert)
                                     ],
-                                  )),
-                                  const VerticalDivider(
-                                    width: 0,
-                                    color: AppColors.white,thickness: 1,
                                   ),
-                                  Expanded(child: Column(
+                                  const SizedBox(
+                                    height: 10,
+                                  ),
+                                  const Divider(
+                                    height: 0,
+                                    thickness: 1,
+                                    color: AppColors.white,
+                                  ),
+                                  Row(
                                     children: [
-                                      Text("${user.location.country} ${user.location.city} ${user.location.street}"),
+                                      AppCacheImage(
+                                        imageUrl: user.picture.medium,
+                                        height: 60,
+                                        round: 60,
+                                      ),
+                                      const SizedBox(width: 10),
+                                      Text(
+                                          "${user.name.title}, ${user.name.first} ${user.name.last} - ${user.dob.age}"),
                                     ],
-                                  )),
+                                  ),
+                                  const SizedBox(
+                                    height: 10,
+                                  ),
+                                  Row(
+                                    children: [
+                                      Expanded(
+                                          child: Column(
+                                        children: [
+                                          Text(user.dob.date),
+                                        ],
+                                      )),
+                                      const VerticalDivider(
+                                        width: 0,
+                                        color: AppColors.white,
+                                        thickness: 1,
+                                      ),
+                                      Expanded(
+                                          child: Column(
+                                        children: [
+                                          Text(
+                                              "${user.location.country} ${user.location.city} ${user.location.street}"),
+                                        ],
+                                      )),
+                                    ],
+                                  ),
                                 ],
                               ),
-                            ],
-                          ),
-                        );
-                      });
+                            );
+                          });
                 },
               ))
             ],
